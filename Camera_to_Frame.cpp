dd60fd34f5b71c9f1d45977c7752c2b0f53e95e9
lnums <ms15941699808@163.com>
2014-11-18 16:00:39 +0800

    Update Camera_to_Frame.cpp

--- Camera_to_Frame.cpp
+++ Camera_to_Frame.cpp
@@ -17,7 +17,8 @@ int main(int argc,char* argv[])
 {
         CvCapture* capture1 = cvCreateCameraCapture( 0  );
         CvCapture* capture2 = cvCreateCameraCapture( 1  );
-
+	const char* filename1="capture1.avi";
+	const char* filename2="capture2.avi";
         double w = 500, h = 320;
         cvSetCaptureProperty ( capture1, CV_CAP_PROP_FRAME_WIDTH,  w  );  
         cvSetCaptureProperty ( capture1, CV_CAP_PROP_FRAME_HEIGHT, h  );
@@ -29,18 +30,23 @@ int main(int argc,char* argv[])
 
         IplImage* frame1;
         IplImage* frame2;
-
+        frame1 = cvQueryFrame( capture1  );
+        frame2 = cvQueryFrame( capture2  );
+        CvVideoWriter *writer1 =cvCreateVideoWriter(filename1, -1, 25, cvSize(frame1->width,frame->height));//create writer 
+        CvVideoWriter *writer2 =cvCreateVideoWriter(filename2, -1, 25, cvSize(frame2->width,frame->height));//create writer 
         int n = 2;
         while(1)
     {
                 frame1 = cvQueryFrame( capture1  );
                 if( !frame1  ) break;
                 cvSaveImage("./frame1/frame1.jpg",frame1);
+                cvWriteFrame(writer1,frame1);
                 cvShowImage( "Camera_1", frame1  );
 
                 frame2 = cvQueryFrame( capture2  );
                 if( !frame2  ) break;
-                 cvSaveImage("./frame2/frame2.jpg",frame2);
+                cvSaveImage("./frame2/frame2.jpg",frame2);
+                cvWriteFrame(writer2,frame);
                 cvShowImage( "Camera_2", frame2  );
 
                 int key = cvWaitKey(30);
@@ -49,6 +55,8 @@ int main(int argc,char* argv[])
     }
         cvReleaseCapture( &capture1  );
         cvReleaseCapture( &capture2  );
+        cvReleaseVideoWriter(&writer1);
+        cvReleaseVideoWriter(&writer2);
         cvDestroyWindow( "Camera_1"  );
         cvDestroyWindow( "Camera_2"  );
 
