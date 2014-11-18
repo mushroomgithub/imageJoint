b06a6a3dcab5605408a0adfac2d1bac6b8f0bef0
lnums <ms15941699808@163.com>
2014-11-18 18:09:48 +0800

    Update Camera_to_Frame.cpp
    add Capture Vedio Function

--- Camera_to_Frame.cpp
+++ Camera_to_Frame.cpp
@@ -29,8 +29,8 @@ int main(int argc,char* argv[])
 		return -1;  
 	}  
 	
-	const char* filename1="capture1.avi";
-	const char* filename2="capture2.avi";
+	const char* filename1="./capture1.avi";
+	const char* filename2="./capture2.avi";
         double w = 500, h = 320;
         cvSetCaptureProperty ( capture1, CV_CAP_PROP_FRAME_WIDTH,  w  );  
         cvSetCaptureProperty ( capture1, CV_CAP_PROP_FRAME_HEIGHT, h  );
@@ -44,8 +44,8 @@ int main(int argc,char* argv[])
         IplImage* frame2;
         frame1 = cvQueryFrame( capture1  );
         frame2 = cvQueryFrame( capture2  );
-        CvVideoWriter *writer1 =cvCreateVideoWriter(filename1, -1, 25, cvSize(frame1->width,frame1->height));//create writer 
-        CvVideoWriter *writer2 =cvCreateVideoWriter(filename2, -1, 25, cvSize(frame2->width,frame2->height));//create writer 
+        CvVideoWriter *writer1 =cvCreateVideoWriter(filename1, CV_FOURCC('M','J','P','G'), 25, cvSize(frame1->width,frame1->height));//create writer 
+        CvVideoWriter *writer2 =cvCreateVideoWriter(filename2, CV_FOURCC('M','J','P','G'), 25, cvSize(frame2->width,frame2->height));//create writer 
         int n = 2;
         while(1)
     {
@@ -58,7 +58,7 @@ int main(int argc,char* argv[])
                 frame2 = cvQueryFrame( capture2  );
                 if( !frame2  ) break;
                 cvSaveImage("./frame2/frame2.jpg",frame2);
-                cvWriteFrame(writer2,frame);
+                cvWriteFrame(writer2,frame2);
                 cvShowImage( "Camera_2", frame2  );
 
                 int key = cvWaitKey(30);
